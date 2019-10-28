5f249193bd73bfb3438c54d18a48aa2efef82f36
github username <randyherasme6@gmail.com>
2019-10-28 12:18:07 -0400

    cbunk finished finally

--- chunk.js
+++ chunk.js
@@ -8,12 +8,13 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(arr, n) {
+function chunk(array, n) {
   let chunk = [];
-  for (let elem of arr) {
+
+  for (let elem of array) {
     let last = chunk[chunk.length - 1];
 
-    if (!last || last.length === n) {
+    if (!last || last.length == n) {
       chunk.push([elem]);
     } else {
       last.push(elem);
