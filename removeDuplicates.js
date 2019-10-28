b17d4eddeaba34d5e9fe8452d7ff750a4e5bea5b
github username <randyherasme6@gmail.com>
2019-10-28 10:20:16 -0400

    remove dupes compelte

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,3 +1,15 @@
 // --- Directions
 // Write a function that removes duplicates
 // from an array
+
+function removeDuplicates(arr) {
+  let output = [];
+  for (let elem of arr) {
+    if (!output.includes(elem)) {
+      output.push(elem);
+    }
+  }
+  return output;
+}
+
+console.log(removeDuplicates([1, 1, 2, 3, 3, 4, 5, 6, 6, 6, 6, 7, 8, 9, 9]));
