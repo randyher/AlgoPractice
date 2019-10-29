5ba2c1e7a3a10b10996c1356f15fa575e897b5f9
github username <randyherasme6@gmail.com>
2019-10-29 15:59:55 -0400

    complete mode

--- meanMedianMode.js
+++ meanMedianMode.js
@@ -3,6 +3,7 @@
 
 const numbers = [11, 12, 12, 13, 14, 14, 15];
 const numbersTwo = [15, 11, 23, 45, 78, 20];
+const numbersThree = [1, 2, 3, 4, 5, 5, 5, 6, 6, 6, 6, 6, 7, 8, 8, 9, 9, 9];
 
 function mean(array) {
   let sum = array.reduce((agg, item) => agg + item);
@@ -19,5 +20,27 @@ function median(array) {
   }
 }
 
+function mean(array) {
+  let map = {};
+  let count = 0;
+  let mostAppeared;
+  for (let num of array) {
+    if (map[num]) {
+      map[num]++;
+    } else {
+      map[num] = 1;
+    }
+  }
+
+  for (let num in map) {
+    if (map[num] > count) {
+      count = map[num];
+      mostAppeared = num;
+    }
+  }
+  return mostAppeared;
+}
+
 // console.log(mean(numbers));
-console.log(median(numbersTwo));
+// console.log(median(numbersTwo));
+console.log(mean(numbersThree));
