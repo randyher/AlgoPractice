89c641939f5989d78a132a3f61ea4c1f70e857dc
github username <randyherasme6@gmail.com>
2019-10-30 13:55:27 -0400

    add anagram

---
+++ anagram.js
@@ -0,0 +1,22 @@
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  return charMap;
+}
