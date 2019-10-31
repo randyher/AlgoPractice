27e8aecdd4194720ea32a0cdf3835dc1b7e606e7
github username <randyherasme6@gmail.com>
2019-10-31 10:08:02 -0400

    queue completed

---
+++ queue.js
@@ -0,0 +1,30 @@
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(input) {
+    this.data.unshift(input);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+const q = new Queue();
+q.add(1);
+q.add(2);
+q.add(3);
+console.log(q.remove());
+console.log(q.data);
