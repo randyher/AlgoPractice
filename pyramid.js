5f328f9faf2140d8c385ff9178b1c7314c374f62
github username <randyherasme6@gmail.com>
2019-11-01 10:33:51 -0400

    finish pyramid

--- pyramid.js
+++ pyramid.js
@@ -13,3 +13,22 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+
+function pyramid(n) {
+  const midpoint = Math.floor((n * 2 - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+
+    console.log(level);
+  }
+}
+
+console.log(pyramid(8));
