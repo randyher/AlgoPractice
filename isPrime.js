78d7ee0bc43deb0a8e4c05b01fcb33ca1ab8b2e3
github username <randyherasme6@gmail.com>
2019-11-03 13:08:54 -0500

    is prime code

--- isPrime.js
+++ isPrime.js
@@ -1,2 +1,15 @@
 // --- Directions
 // Given a number, find out if it's prime
+
+function isPrime(n) {
+  var divisor = 2;
+
+  while (n > divisor) {
+    if (n % divisor == 0) {
+      return false;
+    } else divisor++;
+  }
+  return true;
+}
+
+isPrime(137);
