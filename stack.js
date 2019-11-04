79af077011f754b4ab3a29e66c99d57ab952169d
github username <randyherasme6@gmail.com>
2019-11-04 13:33:39 -0500

    stack code

--- stack.js
+++ stack.js
@@ -5,7 +5,25 @@
 // store it until it is removed.
 // --- Examples
 //   const s = new Stack();
-//   s.push(1);
-//   s.push(2);
+//   s.add(1);
+//   s.add(2);
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.push(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
