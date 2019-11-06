f75fbff5c98117cbeaf74076b858657d91469e6d
github username <randyherasme6@gmail.com>
2019-11-06 14:31:43 -0500

    linkedlist clean up

--- pyramid.js
+++ pyramid.js
@@ -31,4 +31,24 @@ function pyramid(n) {
   }
 }
 
-console.log(pyramid(8));
+//
+
+function pyramidNew(n) {
+  console.log(n);
+  let midpoint = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+}
+
+//
+console.log(pyramidNew(8));
