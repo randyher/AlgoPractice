1e0fbffd0eeed0cf71b1a16ff5f2655f64326293
github username <randyherasme6@gmail.com>
2019-11-25 11:49:15 -0500

    ds readme

--- trees.js
+++ trees.js
@@ -45,13 +45,13 @@ class Tree {
     }
   }
 
-  traverseDF(fn) {
-    const arr = [this.root];
-    while (arr.length) {
-      const node = arr.shift();
+  // traverseDF(fn) {
+  //   const arr = [this.root];
+  //   while (arr.length) {
+  //     const node = arr.shift();
 
-      arr.unshift(...node.children);
-      fn(node);
-    }
-  }
+  //     arr.unshift(...node.children);
+  //     fn(node);
+  //   }
+  // }
 }
